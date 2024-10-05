403208768270a693cc06d307dd4670efe7083bf7
Stefan Reichhart <stefan.reichhart@gmail.com>
2024-10-05 11:14:02 +0200

    history clean

--- macos/cleanup.bash
+++ macos/cleanup.bash
@@ -3,8 +3,9 @@
 # cleanup shortcuts
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-alias xclean='find . -name "*DS_Store" -print -delete; find . -name "._*" -print -delete; rm "$HOME/.zsh_history"'
+alias xclean='find . -name "*DS_Store" -print -delete; find . -name "._*" -print -delete'
 alias xcleanquiet='find . -name "*DS_Store" -delete; find . -name "._*" -delete'
+alias xcodeclean='rm -rdf ~/Library/Developer/Xcode/DerivedData/'
+alias xhistclean='rm "$HOME/.zsh_history"'
 alias jclean='find . -name "*.class" -delete'
 alias pyclean='find . -name "*.pyc" -delete'
-alias xcodeclean='rm -rdf ~/Library/Developer/Xcode/DerivedData/'
